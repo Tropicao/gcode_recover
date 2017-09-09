dca7d6e4155973bc2b36565836961b34fc396afb
Alexis Lothoré <alexis.lothore@gmail.com>
2017-09-10 00:17:09 +0200

    First working version

--- gcode_recover.py
+++ gcode_recover.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #title           :gcode_recover.py
-#description     :Based on last filament output, it will create a new GCode file to resume an interrupted print
+#description     :Based on last filament output, it will create a new G-code file to resume an interrupted print
 #author          :Alexis Lothore
 #date            :20170909
 #version         :0.1
@@ -31,10 +31,76 @@ class RecoverClass:
         else:
             return True
 
+    def getInterruptedLine(self):
+        pattern = re.compile(r'{}'.format(re.escape(self.filament)))
+        with open(self.filepath) as complete_gcode:
+            nb_line = 0
+            found = False
+            for line in complete_gcode:
+                if(pattern.search(line)):
+                    found = True
+                    break
+                else:
+                    nb_line = nb_line+1
+        if(found == True):
+            return nb_line
+        else:
+            return -1
+
+    def getHeightAtInterruption(self):
+        pattern = re.compile(r'Z[0-9]+(\.[0-9]+)?$')
+        with open(self.filepath) as complete_gcode:
+            nb_line = 0
+            current_height = ""
+            for line in complete_gcode:
+                res = pattern.search(line)
+                if(res):
+                    current_height = res.group()
+                else:
+                    nb_line = nb_line+1
+                if nb_line >= self.interrupted_line:
+                    break
+        return current_height
+
+    def writeRecoverFile(self, path):
+        complete_gcode = open(self.filepath, "r")
+        recovery_gcode = open(path, "w")
+
+        recovery_gcode.write("G0 F9000 Z25\n")
+        recovery_gcode.write("G92 {}\n".format(self.filament))
+        nb_line = 0
+        for line in complete_gcode:
+            if nb_line == self.interrupted_line+1 :
+                recovery_gcode.write("{} {}\n".format(line.strip('\n'), self.interrupted_height))
+            elif nb_line > self.interrupted_line+1 :
+                recovery_gcode.write(line)
+            nb_line = nb_line +1
+
+        recovery_gcode.close()
+        complete_gcode.close()
+
+    def createRecoverFile(self):
+        self.interrupted_line = self.getInterruptedLine()
+        if(self.interrupted_line >= 0):
+            print "Interruption occurred at line {}".format(self.interrupted_line)
+        else:
+            print "Interruption line not found, abort"
+            return
+        self.interrupted_height = self.getHeightAtInterruption()
+        if(self.interrupted_height >=0):
+            print "Interruption occurred at height {}".format(self.interrupted_height)
+        else:
+            print "Interruption height not found, abort"
+            return
+
+        recovery_path = "recovery.gcode"
+        self.writeRecoverFile("recovery.gcode")
+        print "New G-code file available at {}".format(os.path.abspath(recovery_path))
+
 def help_usage():
     print "Usage : python {} EXXXX FILE".format(sys.argv[0])
     print "> EXXXX : current filament, example : E187.52"
-    print "> FILE  : the original and complete GCode file"
+    print "> FILE  : the original and complete G-code file"
 
 if __name__ == "__main__":
     if(len(sys.argv) != 3):
@@ -44,7 +110,9 @@ if __name__ == "__main__":
     try:
         recover = RecoverClass(sys.argv[1], sys.argv[2])
     except RecoverException as e:
-        print("Cannot start GCode recovering process : wrong arguments")
+        print("Cannot start G-code recovering process : wrong arguments")
         help_usage()
         exit(1)
-    print("Starting recovering Gcode at interrupted step")
+
+    print("Starting G-code recovering")
+    recover.createRecoverFile()
