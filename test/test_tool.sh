763373856116341d0627992632cbfabfa4fee654
Alexis Lothoré <alexis.lothore@gmail.com>
2017-11-10 20:19:47 +0100

    Improve tester

--- test/test_tool.sh
+++ test/test_tool.sh
@@ -3,4 +3,12 @@
 TEST_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TEST_DATA_DIR=${TEST_DIR}/test_data
 TOOL_DIR=${TEST_DIR}/..
-python ${TOOL_DIR}/gcode_recover.py E1857.38 ${TEST_DATA_DIR}/pristine_gcode.gcode
+TOOL_BIN="gcode_recover.py"
+
+INPUT_FILAMENT="E8082.66163"
+INPUT_FILE="pristine_gcode.gcode"
+INPUT_FILE_PATH=${TEST_DATA_DIR}/${INPUT_FILE}
+
+echo "Filament level : ${INPUT_FILAMENT}"
+echo "Gcode file : ${INPUT_FILE_PATH}"
+python ${TOOL_DIR}/${TOOL_BIN} ${INPUT_FILAMENT} ${INPUT_FILE_PATH}
